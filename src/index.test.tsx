4bcfb54e8050040c7aedae43def3a2e2624a4e27
Chris Cowan <agentme49@gmail.com>
2019-05-03 15:29:16 -0700

    Initial version

---
+++ src/index.test.tsx
@@ -0,0 +1,66 @@
+/* @flow */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Kefir, { Observable } from 'kefir';
+import kefirBus from 'kefir-bus';
+
+import { useKefir, useSyncKefir } from '.';
+
+for (const useTestKefir of [useKefir, useSyncKefir]) {
+  // prevent accidental use
+  const useKefir = null,
+    useSyncKefir = null;
+
+  describe(useTestKefir.name, () => {
+    test('constant', () => {
+      function Foo(props: {}) {
+        const value = useTestKefir(Kefir.constant('abc'));
+        return <div>{value || '<no value>'}</div>;
+      }
+
+      const div = document.createElement('div');
+      act(() => {
+        ReactDOM.render(<Foo />, div);
+      });
+      expect(div.textContent).toBe('abc');
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('initial and multiple values', () => {
+      let isActive = false;
+      let emitter: any;
+      let stream: Observable<string, never> = Kefir.stream(_em => {
+        emitter = _em;
+        isActive = true;
+        return () => {
+          isActive = false;
+        };
+      });
+
+      function Foo(props: {}) {
+        const value = useTestKefir(stream, 'initial');
+        return <div>{value}</div>;
+      }
+
+      const div = document.createElement('div');
+      act(() => {
+        ReactDOM.render(<Foo />, div);
+      });
+      expect(isActive).toBe(true);
+      expect(div.textContent).toBe('initial');
+      act(() => {
+        emitter.value('abc');
+      });
+      expect(div.textContent).toBe('abc');
+      act(() => {
+        emitter.value('123');
+      });
+      expect(div.textContent).toBe('123');
+      expect(isActive).toBe(true);
+      ReactDOM.unmountComponentAtNode(div);
+      expect(isActive).toBe(false);
+    });
+  });
+}
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+/* @flow */
+
+import { Observable } from 'kefir';
+import { useState, useEffect, useLayoutEffect, DependencyList } from 'react';
+
+// There is a slice of time between the initial value is received and the stream is
+// subscribed to. If it's important to not miss changes that may happen in this slice
+// of time, make the stream be a property (`.toProperty(() => currentValue)`) so that
+// the most current value is received immediately on subscription.
+
+// This subscribes to the stream asynchronously after the component has been
+// mounted and visible to the user. Use `useSyncKefir` if it's important that the
+// stream is subscribed to before the user sees the component rendered.
+export function useKefir<T>(stream: Observable<T, any>): T | void;
+export function useKefir<T>(
+  stream: Observable<T, any>,
+  initialValue: T,
+  inputs?: DependencyList
+): T;
+export function useKefir<T>(
+  stream: Observable<T, any>,
+  initialValue?: T,
+  inputs?: DependencyList
+): T | void {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    stream.onValue(setValue);
+    return () => {
+      stream.offValue(setValue);
+    };
+  }, inputs);
+
+  return value;
+}
+
+// Use this if it's important that the stream is subscribed to before first render.
+export function useSyncKefir<T>(stream: Observable<T, any>): T | void;
+export function useSyncKefir<T>(
+  stream: Observable<T, any>,
+  initialValue: T,
+  inputs?: DependencyList
+): T;
+export function useSyncKefir<T>(
+  stream: Observable<T, any>,
+  initialValue?: T,
+  inputs?: DependencyList
+): T | void {
+  const [value, setValue] = useState(initialValue);
+
+  useLayoutEffect(() => {
+    stream.onValue(setValue);
+    return () => {
+      stream.offValue(setValue);
+    };
+  }, inputs);
+
+  return value;
+}
